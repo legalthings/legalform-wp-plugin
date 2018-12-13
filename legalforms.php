a498c5ee371263550d7cd069253e36d5d7daddd1
Jurre Wolsink <jurre.wolsink@gmail.com>
2018-12-13 17:27:02 +0100

    Update version

--- legalforms.php
+++ legalforms.php
@@ -3,7 +3,7 @@
 /*
  *   Plugin Name: LegalThings Legalforms
  *   Description: This plugin can automatically create a LegalThings LegalForm in a page by some shortcode
- *   Version: 1.7.1
+ *   Version: 1.7.2
  *   Author: LegalThings
  */
 
